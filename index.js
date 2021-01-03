16de331565af98c9ec68010ac011c477b70dd93a
john gravois <jagravois@gmail.com>
2021-01-02 21:08:53 -0800

    lots of CRUD without much boilerplate

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const finale = require('finale-rest')
 
 const uri = `postgres://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASS}@localhost:5433/${process.env.ATLAS_DB}`
 
 const sequelize = new Sequelize(uri)
 
-class LosAngelesAnnexation extends Model {}
+const PORT = process.env.ATLAS_DB_PORT || 3001
 
-LosAngelesAnnexation.init({
+class JournalEntry extends Model {}
+
+JournalEntry.init({
   // Model attributes are defined here
-  name: {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  memo: {
     type: DataTypes.STRING
   },
   geom: {
@@ -18,26 +27,42 @@ LosAngelesAnnexation.init({
 }, {
   // Other model options go here
   sequelize, // We need to pass the connection instance
-  tableName: 'los_angeles_annexations',
-
-  // since sequelize didnt create the table, its missing createdAt/updatedAt
-  timestamps: false,
-  modelName: 'LosAngelesAnnexation' // We need to choose the model name
+  modelName: 'JournalEntry' // We need to choose the model name
 });
 
+const restify = require('restify');
+const corsMiddleware = require('restify-cors-middleware');
 
-const go = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    const payload = await LosAngelesAnnexation.findAll();
-    payload.forEach(result => {
-      console.log(result.geom)
-    })
-    await sequelize.close();
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+var app, server;
+app = server = restify.createServer()
 
-go()
+const cors = corsMiddleware({
+  preflightMaxAge: 5, // Optional
+  origins: ['*'], // Should whitelist actual domains in production
+  allowHeaders: ['Authorization', 'API-Token', 'Content-Range'], //Content-range has size info on lists
+  exposeHeaders: ['Authorization', 'API-Token-Expiry', 'Content-Range']
+})
+
+server.pre(cors.preflight)
+server.use(cors.actual)
+
+server.use(restify.plugins.queryParser()); //{mapParams: true}
+server.use(restify.plugins.bodyParser());  //{mapParams: true, mapFiles: true}
+server.use(restify.plugins.acceptParser(server.acceptable));
+
+// Initialize finale
+finale.initialize({
+  app: app,
+  sequelize
+});
+
+// Create REST resource
+finale.resource({
+  model: JournalEntry,
+  endpoints: ['/journal-entries', '/journal-entries/:id']
+});
+
+// calling sequelize.sync() blows away the table each time the server restarts
+server.listen(PORT, function() {
+  console.log(`listening at http://localhost:${PORT}`);
+});
